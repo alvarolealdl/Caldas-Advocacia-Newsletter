93a84ad85e15565d0190eb6c5542bb8bfda980e6
Álvaro Leal <alvaro.leal@liferay.com>
2024-08-05 21:27:24 -0300

    Chore: Add some new dependencies and configurations to package.json, vite.config and eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslint({
+      emitError: true,
+      failOnError: false,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+});
