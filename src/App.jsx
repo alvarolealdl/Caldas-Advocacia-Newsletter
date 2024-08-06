fc09475694c163264cd56a27e49085de156b7c9f
Álvaro Leal <alvaro.leal@liferay.com>
2024-08-05 21:29:42 -0300

    Feat: Add some new routes to show pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Home from "./pages/home";
+import Form from "./pages/form";
+
 const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/form" element={<Form />} />
+    </Routes>
+  );
 };
 
 export default App;
