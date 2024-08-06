ec49ed3badff0180a6357c4b7c69257df3194e7f
Álvaro Leal <alvaro.leal@liferay.com>
2024-08-05 21:30:01 -0300

    Feat: Add google form component

---
+++ src/pages/form.jsx
@@ -0,0 +1,22 @@
+const Form = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        overflowX: "hidden",
+      }}
+    >
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeVdfoQHVKIBD6yaoB0TSJt3PvfqN6CLTn6Ddj5qr8iNz287A/viewform?embedded=true"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Form;
